1d1b2cf9293ea00ff0636580021759869eed61ff
Anakha T.A <76938762+sngca09@users.noreply.github.com>
2021-09-07 20:48:09 +0530

    Add files via upload

--- product.java
+++ product.java
@@ -1,85 +1,57 @@
-import java.util.Scanner;
-public class product
+import java.util.*;
+interface order
 {
- int pcode,price;
- String pname=""; 
-void read()
-{
- Scanner sc=new Scanner(System.in);
- System.out.println("Enter the product code:");
- pcode=sc.nextInt();
- System.out.println("Enter the product name:");
- pname=sc.next();
- System.out.println("Enter the product price:");
- price=sc.nextInt();
+    void order();
 }
-void disp()
-{
- System.out.println("\n\t\tPRODUCT DETAILS");
- System.out.println("\t\t---------------\n");
- System.out.println("Product Code"+pcode);
- System.out.println("Product Name:"+pname);
- System.out.println("Product Price:"+price);
- }
- public static void main(String args[])
-{
- product p1=new product();
- product p2=new product();
- product p3=new product();
-System.out.println("Enter the first product details:\n");
- p1.read();
-System.out.println("Enter the second product details:\n");
- p2.read();
-System.out.println("Enter the third product details:\n");
- p3.read();
-if(p1.price<p2.price && p1.price<p3.price)
-{
-     System.out.println("\n "+p1.price+" is smallest price");
-     p1.disp();
-   }
-else if(p2.price<p3.price)
-{
-   System.out.println("\n "+p2.price+" is smallest price");
-   p2.disp();
- }
-else 
- {
-   System.out.println("\n "+p3.price+" is smallest price");
-   p3.disp();
-  }
- }
+class product implements order {
+    Scanner sc = new Scanner(System.in);
+    int pid,quant,price,totals,ord_no;
+    String date,name;
+    public void order()
+    {
+        System.out.println("Enter the product details:");
+        System.out.println("DATE:");
+        date=sc.next();
+        System.out.println("Order number:");
+        ord_no=sc.nextInt();
+        System.out.println("Product Id:");
+        pid=sc.nextInt();
+        System.out.println("Name:");
+        name=sc.next();
+        System.out.println("Quantity:");
+        quant=sc.nextInt();
+        System.out.println("Unit price:");
+        price=sc.nextInt();
+        System.out.println("Total:"+(price*quant));
+    }
+    void display()
+    {
+        System.out.println("Details are:");
+        System.out.println("DATE:"+date);
+        System.out.println("Order number:"+ord_no);
+        System.out.println("Product Id:"+pid);
+        System.out.println("Name:"+name);
+        System.out.println("Quantity:"+quant);
+        System.out.println("price:"+price);
+        System.out.println("Total:"+(price*quant));
+
+    }
+
+    public static void main(String ...a)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the limit:");
+        int n=sc.nextInt();
+        int sum=0,te=0;
+        product p[]=new product[n];
+        for (int i=0;i<n;i++)
+        {
+            p[i]= new product();
+            p[i].order();
+            p[i].display();
+            sum+=p[i].price*p[i].quant;
+        }
+        te+=sum;
+        System.out.println("Grand Total:"+sum);
+    }
 }
-/*op
-Enter the first product details:
-
-Enter the product code:
-1
-Enter the product name:
-laptop
-Enter the product price:
-55000
-Enter the second product details:
-
-Enter the product code:
-2
-Enter the product name:
-pen
-Enter the product price:
-400
-Enter the third product details:
-
-Enter the product code:
-3
-Enter the product name:
-pencil
-Enter the product price:
-2
-
- 2 is smallest price
-
-                PRODUCT DETAILS
-                ---------------
-
-Product Code3
-Product Name:pencil
-Product Price:2    */
